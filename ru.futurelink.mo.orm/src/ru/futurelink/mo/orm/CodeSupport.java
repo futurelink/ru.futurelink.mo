c5c1ab0fc18ea356917a26489fa1ec6fd1203525
Pavlov Denis <me@futurelink.ru>
2014-11-05 16:57:27 +1100

    Added copyright headers to all sources.

--- ru.futurelink.mo.orm/src/ru/futurelink/mo/orm/CodeSupport.java
+++ ru.futurelink.mo.orm/src/ru/futurelink/mo/orm/CodeSupport.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2013-2014 Pavlov Denis
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Pavlov Denis - initial API and implementation
+ ******************************************************************************/
+
 package ru.futurelink.mo.orm;
 
 import javax.persistence.Column;
@@ -9,11 +20,7 @@ import javax.persistence.Table;
 import javax.persistence.JoinColumn;
 
 /**
- * ��������� ����� ���������.
- * 
- * ������ ������ �������� ��� �������� � ���� ����������������� ID,
- * �������� ������ �������� � ������� ID ����� �������� � ����� �������.
- * 
+ *
  * @author Futurelink
  * @since 0.1
  *
